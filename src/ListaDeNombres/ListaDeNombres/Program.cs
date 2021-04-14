e58ff9ad195e5ab5469ff3a69fa9c3b85820e23d
SeijasTomas <43001983+SeijasTomas@users.noreply.github.com>
2021-04-14 19:19:26 -0300

    Agrego ejercicio lista de nombres

---
+++ src/ListaDeNombres/ListaDeNombres/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListaDeNombres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nombres = new List<string>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Ingrese nombre " + i + " :");
+                string nombre;
+                nombre = Console.ReadLine();
+                nombres.Add(nombre);
+            }
+            Console.WriteLine("Nombres ordenados alfabeticamente:");
+            nombres.Sort();
+
+            foreach (string item in nombres)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
